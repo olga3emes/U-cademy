a2819cf9324c4565d03bdff843319fbb1c234530
samsiesil <sam935390@gmail.com>
2015-03-14 15:39:34 +0100

    Error solucionado: faltaban restricciones en fecha de la entidad File.
    Se me olvidó añadir las restricciones del atributo uploadMoment, ya se
    han añadido.

--- src/main/java/domain/File.java
+++ src/main/java/domain/File.java
@@ -7,10 +7,14 @@ import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -51,6 +55,10 @@ public class File extends DomainEntity{
 		this.mediaType = mediaType;
 	}
 	
+	@Past
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern="dd/MM/yyyy HH:mm")
 	public Date getUploadMoment() {
 		return uploadMoment;
 	}
